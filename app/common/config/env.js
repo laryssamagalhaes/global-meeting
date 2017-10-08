253194a7aacf54f7160ddd84e3b240cf5de463ba
laryssamagalhaes <laryssamagal@gmail.com>
2017-10-08 14:25:38 -0300

    gh-pages

--- app/common/config/env.js
+++
@@ -1,3 +0,0 @@
-angular.module("env", [])
-.constant("API_URL", "https://maps.googleapis.com/maps/api/timezone/json")
-.constant("KEY", "AIzaSyBdKcOOJljK0GCbqaM5EzsSkJorY7ZLsFo");
--- tasks/watch.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('watch', function () {
-  gulp.watch('app/**/*.js', ['js', 'refresh']);
-  gulp.watch('app/assets/styles/**/*.scss', ['css', 'refresh']);
-  gulp.watch('app/**/*.html', ['html', 'refresh']);
-});
